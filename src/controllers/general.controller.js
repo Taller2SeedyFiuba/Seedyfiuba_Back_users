1509f0581a308ade84d42e8b3541eb7c1ebf0813
JulianVentura <julianv6004@gmail.com>
2021-05-05 12:45:54 -0300

    Revierto cambios de main

--- src/controllers/general.controller.js
+++
@@ -1,8 +0,0 @@
-import { sequelize } from "../database/database";
-
-export async function getDatabaseStatus(req, res) {
-    await sequelize.authenticate();
-    res.status(200).json({
-        "message": "Connection with database has been established successfully."
-    })
-}
